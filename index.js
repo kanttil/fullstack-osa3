cbfba3081a20e6e2a0b05b8e1e464193169461c0
Kamilla Anttila <kamilla.anttila@gmail.com>
2018-08-16 13:49:50 +0300

    Initiate app.

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+morgan.token('data', function getData(req) {
+    return JSON.stringify(req.body)
+})
+
+app.use(bodyParser.json())
+app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms'))
+app.use(cors())
+
+let persons = [
+    {
+        name: 'Arto Hellas',
+        number: '040-123456',
+        id: 1
+    },
+    {
+        name: 'Martti Tienari',
+        number: '040-123456',
+        id: 2
+    },
+    {
+        name: 'Arto Järvinen',
+        number: '040-123456',
+        id: 3
+    },
+    {
+        name: 'Lea Kutvonen',
+        number: '040-123456',
+        id: 4
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>puhelinluettelossa ${persons.length} henkilön tiedot</p>` + new Date())
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (body.name === undefined) {
+        return response.status(400).json({ error: 'name missing' })
+    } else if (body.number === undefined) {
+        return response.status(400).json({ error: 'number missing' })
+    } else if (persons.map(person => person.name).includes(body.name)) {
+        return response.status(400).json({ error: 'name must be unique' })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 1000) + 1
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
